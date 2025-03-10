a75dcca1aa4dc713b1aaa7fbb0b3185114eaab99
Sneha <sneharn3@gmail.com>
2025-03-10 23:42:34 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ from lime import lime_image
 from dotenv import load_dotenv
 
 st.set_page_config(initial_sidebar_state="collapsed")
+hide_menu_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 st.title("🌱 Plant Disease Detection")
 
